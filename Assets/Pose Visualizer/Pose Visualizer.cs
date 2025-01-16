f2ad8500086d674251c886ba18c72ebd28ee03f6
Sombra <lyccevt@gmail.com>
2025-01-16 12:08:01 +0800

    WIP

--- Assets/Pose Visualizer/Pose Visualizer.cs
+++ Assets/Pose Visualizer/Pose Visualizer.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Serialization;
 using Debug = UnityEngine.Debug;
+using Gusto;
 
 public class PoseVisualizer : MonoBehaviour
 {
@@ -21,7 +24,11 @@ public class PoseVisualizer : MonoBehaviour
 
     private static readonly float CAMERA_WIDTH = 800f;
     private static readonly float CAMERA_HEIGHT = 448f;
-
+    public IntPtr tracker;
+    float[] result_pose = new float[16];
+    float[] confidences = new float[1];
+    WebCamTexture webcamTexture;
+    [SerializeField] RawImage m_rawImage;
     int _frameIndex = 0;
 
     // Start is called before the first frame update
@@ -41,51 +48,80 @@ public class PoseVisualizer : MonoBehaviour
 
             Debug.Log($"tl = {tl}, br = {br}");
         }
+        WebCamDevice[] devices = WebCamTexture.devices;
+        webcamTexture = new WebCamTexture();
+
+        if (devices.Length > 0)
+        {
+            webcamTexture.deviceName = devices[0].name;
+            webcamTexture.Play();
+        }
+        Debug.Log("Webcam texture height: " + webcamTexture.height);
+        Debug.Log("Webcam texture width: " + webcamTexture.width);
+        
+        Matrix4x4 init_pose_ = new Matrix4x4(
+            new Vector4(1f, 0f, 0f, 0f),
+            new Vector4(0f, 0f, -1f, 0f),
+            new Vector4(0f, -1f, 0f, 0f),
+            new Vector4(0f, 0f, 0.8f, 1f)
+        );
+        UpdateOutput(init_pose_);
+
+        Gusto.ModelTarget.GustoModelTargetInit(out tracker, webcamTexture.height, webcamTexture.width);
+
+        float[] init_pose = new float[16];
+        Gusto.ModelTarget.CADModelInit(
+            tracker,
+            "Bruni",
+            Gusto.Utility.retrieve_streamingassets_data("Bruni-woband/Bruni-woband.obj"),
+            Gusto.Utility.retrieve_streamingassets_data("Bruni-woband/cvs/Bruni-woband.meta"),
+            0.6f,
+            0.5f,
+            init_pose
+        );
+
+        // for(int i = 0; i < 16; i++){
+        //     Debug.Log(init_pose[i]);
+        // }
+        Gusto.ModelTarget.TrackerInit(tracker, 70.0f);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_frameIndex % 200 == 0)
+        m_rawImage.texture = webcamTexture;
+
+        Color32[] pixels = webcamTexture.GetPixels32();
+        GCHandle pixelsHandle = GCHandle.Alloc(pixels, GCHandleType.Pinned);
+        IntPtr pixelsPtr = pixelsHandle.AddrOfPinnedObject();
+        Gusto.ModelTarget.track(tracker, pixelsPtr, result_pose, confidences);
+
+        // Debug.Log("Confidence: " + confidences[0]);
+        // Debug.Log($"result_pose = {result_pose[0]}, {result_pose[1]}, {result_pose[2]}, {result_pose[3]}");
+        // Debug.Log($"result_pose = {result_pose[4]}, {result_pose[5]}, {result_pose[6]}, {result_pose[7]}");
+        // Debug.Log($"result_pose = {result_pose[8]}, {result_pose[9]}, {result_pose[10]}, {result_pose[11]}");
+        // Debug.Log($"result_pose = {result_pose[12]}, {result_pose[13]}, {result_pose[14]}, {result_pose[15]}");
+        var output = "";
+        for (int i = 0; i < 16; i++)
         {
-            // Matrix4x4 sample1 = new Matrix4x4(
-            //     new Vector4(-0.985832f, -0.16757f, -0.00175236f, -0.0159864f),
-            //     new Vector4(-0.000519702f, 0.0135599f, -0.999936f, -0.00498361f),
-            //     new Vector4(0.167572f, -0.985784f, -0.0134594f, 0.901313f),
-            //     new Vector4(0f, 0f, 0f, 1f)
-            // );
-            Matrix4x4 sample1 = new Matrix4x4(
-                new Vector4(-0.985832f, -0.000519702f, 0.167572f, 0f),
-                new Vector4(-0.16757f, 0.0135599f, -0.985784f, 0f),
-                new Vector4(-0.00175236f, -0.999936f, -0.0134594f, 0f),
-                new Vector4(-0.0159864f, -0.00498361f, 0.901313f, 1f)
-            );
-            UpdateOutput(sample1);
+            output += result_pose[i] + ", ";
+            // Debug.Log(result_pose[i]);
         }
-        else if (_frameIndex % 200 == 100)
-        {
-            // Matrix4x4 sample2 = new Matrix4x4(
-            //     new Vector4(-0.567041f, -0.822898f, -0.0360135f, 0.33257f),
-            //     new Vector4(0.0277141f, 0.0246396f, -0.999319f, 0.158565f),
-            //     new Vector4(0.823222f, -0.567659f, 0.00883314f, 1.27586f),
-            //     new Vector4(0f, 0f, 0f, 1f)
-            // );
-            // Matrix4x4 sample2 = new Matrix4x4(
-            //     new Vector4(-0.567041f, 0.0277141f, 0.823222f, 0f),
-            //     new Vector4(-0.822898f, 0.0246396f, -0.567659f, 0f),
-            //     new Vector4(-0.0360135f, -0.999319f, 0.00883314f, 0f),
-            //     new Vector4(0.33257f, 0.158565f, 1.27586f, 1f)
-            // );
-            Matrix4x4 sample2 = new Matrix4x4(
-            new Vector4(-0.922999f, -0.350763f, -0.158216f, 0f),
-            new Vector4(0.114801f, 0.141438f, -0.983266f, 0f),
-            new Vector4(0.367274f, -0.925733f, -0.0902832f, 0f),
-            new Vector4(-0.415403f, -0.14886f, 0.962236f, 1f)
-                );
-            UpdateOutput(sample2);
+        Debug.Log($"{output}");
+        if (confidences[0] > 0.8){
+            Matrix4x4 sample = new Matrix4x4(
+                new Vector4(result_pose[0], result_pose[1], result_pose[2], result_pose[3]),
+                new Vector4(result_pose[4], result_pose[5], result_pose[6], result_pose[7]),
+                new Vector4(result_pose[8], result_pose[9], result_pose[10], result_pose[11]),
+                new Vector4(result_pose[12], result_pose[13], result_pose[14], result_pose[15])
+            );
+            UpdateOutput(sample);
         }
 
-        _frameIndex = (_frameIndex + 1) % 200;
+
+
     }
 
     static public void InitOutput(Matrix4x4 matrix)
@@ -107,7 +143,7 @@ public class PoseVisualizer : MonoBehaviour
         Vector3 scale;
         // flip y and z axis
         var matrix2 = matrix * new Matrix4x4(
-            new Vector4(1, 0, 0, 0),
+            new Vector4(-1, 0, 0, 0),
             new Vector4(0, 0, 1, 0),
             new Vector4(0, 1, 0, 0),
             new Vector4(0, 0, 0, 1)
@@ -121,10 +157,10 @@ public class PoseVisualizer : MonoBehaviour
 
         // scale up by 50
         // TODO: no hardcode
-        var matrix4 = matrix3 * Matrix4x4.Scale(new Vector3(-50f, -50f, -50f));
+        // var matrix4 = matrix3 * Matrix4x4.Scale(new Vector3(-1f, -1f, -1f));
 
 
-        Matrix4x4Extension.Decompose(matrix4, out translation, out rotation, out scale);
+        Matrix4x4Extension.Decompose(matrix3, out translation, out rotation, out scale);
         
         rotation = Quaternion.Euler(new Vector3(-rotation.eulerAngles.x, rotation.eulerAngles.y, -rotation.eulerAngles.z));
 
